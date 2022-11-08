c3ae5d5452ae6f9f4ca27ae3d42d9aa1e6ea6c52
qais-alsgher <qaisalsgher@gmail.com>
2022-11-08 00:24:37 +0000

    nice style for system

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ require(`dotenv`).config();
 const server = require(`./server.js`);
 const db = require(`./models/index.js`);
 
-db.sequelize.sync({ force: true }).then(() => {
-// db.sequelize.sync().then(() => {
+// db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync().then(() => {
     server.start(process.env.PORT || 4040);
 }).catch((err) => {
     console.log(err);
